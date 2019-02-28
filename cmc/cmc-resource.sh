a07ddb710cd187ea89f8fbe5df76f2aaf10e99a4
DBE Software M <undisclosed@example.com>
2019-02-28 13:35:14 +0000

    bugfix

--- cmc/cmc-resource.sh
+++ cmc/cmc-resource.sh
@@ -347,7 +347,7 @@ function compute_resources()
 
   kcpmsg "unordered resources ${ip_key[@]}"
 
-  for name in "${!ip_key[@]} | tr ' ' '\n' | sort -n" ; do
+  for name in "echo ${!ip_key[@]} | tr ' ' '\n' | sort -n" ; do
     ordered_boards+=(${ip_key[${name}]})
   done
 
